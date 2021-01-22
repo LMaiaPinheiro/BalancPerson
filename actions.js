c7ba8264fa747b209625d51377792dec861c7fe7
Lucas Pinheiro <77104894+LMaiaPinheiro@users.noreply.github.com>
2021-01-21 22:47:12 -0300

    icons e funcionalidades
    Adição de icone, ajuste de aparencia e criação de funcionalidades

---
+++ actions.js
@@ -0,0 +1,10 @@
+
+    function openModel(){
+        let modal =document.querySelector('.modal-overlay')
+        modal.classList.add('active')
+    }
+    function closeModel(){
+        let modal =document.querySelector('.modal-overlay')
+        modal.classList.remove('active')
+        
+    }
